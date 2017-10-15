72c2d57e32cb45d97187877414890557d9365639
Martin Harari Thuresson <martin@46elks.com>
2017-10-15 11:34:17 +0200

    Färger och aktuellt i listan

--- atportal.py
+++ atportal.py
@@ -6,31 +6,53 @@ from kivy.lang.builder import Builder
 appView = Builder.load_string("""
 <AtPortal>
 GridLayout:
+  spacing: [ 2, 2 ]
   cols: 1
   Button:
     id: f_but
-    text: "Anställning - Ledighet"
+    text: "[color=000000][b]Anställning - Ledighet[/b][/color]"
+    background_color: [ 255, 255, 255, 255 ]
+    markup: True
   Button:
     id: f_buta
-    text: "Tjänstgöring - Schema - Legitimation"
+    text: "[color=000000][b]Tjänstgöring - Schema - Legitimation[/b][/color]"
+    background_color: [ 255, 255, 255, 255 ]
+    markup: True
   Button:
     id: f_butb
-    text: "Utbildning - Kurser"
+    text: "[color=000000][b]Utbildning - Kurser[/b][/color]"
+    background_color: [ 255, 255, 255, 255 ]
+    markup: True
   Button:
     id: f_butc
-    text: "Forskning - Förbättring"
+    text: "[color=000000][b]Forskning - Förbättring[/b][/color]"
+    background_color: [ 255, 255, 255, 255 ]
+    markup: True
   Button:
     id: f_butd
-    text: "Handledning"
+    text: "[color=000000][b]Handledning[/b][/color]"
+    background_color: [ 255, 255, 255, 255 ]
+    markup: True
   Button:
     id: f_bute
-    text: "AT-råd - AT-rum"
+    text: "[color=000000][b]AT-råd - AT-rum[/b][/color]"
+    background_color: [ 255, 255, 255, 255 ]
+    markup: True
   Button:
     id: f_bute
-    text: "Kontakt"
+    text: "[color=000000][b]Kontakt[/b][/color]"
+    background_color: [ 255, 255, 255, 255 ]
+    markup: True
   Button:
     id: f_bute
-    text: "Regionkalendern"
+    text: "[color=000000][b]Regionkalendern[/b][/color]"
+    background_color: [ 255, 255, 255, 255 ]
+    markup: True
+  Button:
+    id: f_bute
+    text: "[color=000000][b]Aktuellt[/b][/color]"
+    background_color: [ 255, 255, 255, 255 ]
+    markup: True
 """)
 
 class ATApp(App):
