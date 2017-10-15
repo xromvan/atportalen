ddff9de8b4dfd90e32192f0ea2e9dcffba0b761f
Martin Harari Thuresson <martin@46elks.com>
2017-10-15 11:08:13 +0200

    Added first basic version

--- README.md
+++ README.md
@@ -2,7 +2,26 @@
 SU AT Portalens APP
 
 # Vi använder Kivy för att skapa appen.
-Finns här https://kivy.org/#home
+Finns här: https://kivy.org/#home
+
+Ladda ner här: https://kivy.org/#download
+
+## För Mac:
+
+  brew install pkg-config sdl2 sdl2_image sdl2_ttf sdl2_mixer gstreamer
+
+  virtualenv atenv
+  source atenv/bin/activate
+  pip install -I Cython==0.25.2
+  pip install kivy
+
+## För Windows:
+
+  python -m pip install --upgrade pip wheel setuptools
+  python -m pip install docutils pygments pypiwin32 kivy.deps.sdl2 kivy.deps.glew
+  python -m pip install kivy.deps.gstreamer
+  python -m pip install kivy.deps.angle
+  python -m pip install kivy
 
 # Appen ska ha
 
--- atportal.py
+++ atportal.py
@@ -1,14 +1,40 @@
-#from kivy.app import App
+# -*- coding: utf-8 -*-
+from kivy.app import App
+from kivy.uix.button import Button
 from kivy.lang.builder import Builder
 
 appView = Builder.load_string("""
 <AtPortal>
 GridLayout:
   cols: 1
-  rows: 1
   Button:
     id: f_but
-    text: "Test"
+    text: "Anställning - Ledighet"
+  Button:
+    id: f_buta
+    text: "Tjänstgöring - Schema - Legitimation"
+  Button:
+    id: f_butb
+    text: "Utbildning - Kurser"
+  Button:
+    id: f_butc
+    text: "Forskning - Förbättring"
+  Button:
+    id: f_butd
+    text: "Handledning"
+  Button:
+    id: f_bute
+    text: "AT-råd - AT-rum"
+  Button:
+    id: f_bute
+    text: "Kontakt"
+  Button:
+    id: f_bute
+    text: "Regionkalendern"
 """)
 
-appView.run()
+class ATApp(App):
+    def build(self):
+        return appView
+
+ATApp().run()
